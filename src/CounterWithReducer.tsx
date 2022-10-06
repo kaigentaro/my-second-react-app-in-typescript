fa4a1c7e6ded461c0f2d6081a8a71165f4205f79
kaigentaro <gengen3509@gmai.com>
2022-10-06 14:38:27 +0900

    increment reset button

--- src/CounterWithReducer.tsx
+++ src/CounterWithReducer.tsx
@@ -1,13 +1,20 @@
 import React, { useReducer, useState } from 'react';
 
+type StateType = { count: number };
 const initialState = { count: 0 };
 
-function reducer(state: any, action: any) {
+type ActionType = {
+  type: 'decrement' | 'increment' | 'reset';
+};
+//reducer：状態を受け取って新たなものを返す
+function reducer(state: StateType, action: ActionType): StateType | never {
   switch (action.type) {
     case 'increment':
       return { count: state.count + 1 };
     case 'decrement':
       return { count: state.count - 1 };
+    case 'reset':
+      return initialState;
     default:
       throw new Error();
   }
@@ -20,6 +27,7 @@ function CounterWithReducer() {
       Count: {state.count}
       <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
       <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>reset</button>
     </>
   );
 }
